e9ed1d44f053d53cf5b9a08924df4b6be61e00a5
andou <antonio.pastorino@gmail.com>
2015-01-12 15:45:26 +0100

    First useless commit

---
+++ index.php
@@ -0,0 +1,137 @@
+<?php
+
+define('PASSPHRASE', '------');
+define('VALID_COOKIE', '-------');
+define('COOKIE_NAME', '_tk');
+define('PWD_ARGUMENT', 't');
+
+bit_access_only_with_cookie_or_password();
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////  WRAPPING  //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Effettua il controllo per una password o per un cookie di password
+ * 
+ */
+function bit_access_only_with_cookie_or_password() {
+  if (isset($_GET[PWD_ARGUMENT])) {
+    $pass = $_GET[PWD_ARGUMENT];
+    if ($pass === PASSPHRASE) {
+      bit_access_valid_pwd();
+    } else {
+      bit_access_invalid_pwd();
+    }
+  } else {
+    if (bit_access_has_valid_cookie()) {
+      bit_access_valid_cookie();
+    } else {
+      bit_access_invalid_cookie();
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////  GESTIONE DELLE ACTIONS  ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Azione da effettuare nel caso in cui la password inserita sia corretta
+ * 
+ * Setta il cookie ed effettua un redirect verso la pagina richiesta
+ * 
+ */
+function bit_access_valid_pwd() {
+  bit_access_set_cookie();
+  bit_access_redirect();
+}
+
+/**
+ * Azione da effettuare in caso di password non corretta
+ * 
+ * Restituisce un 403 Forbidden
+ * 
+ */
+function bit_access_invalid_pwd() {
+  header('HTTP/1.0 403 Forbidden');
+  echo 'Sorry... This page is forbidden (P)';
+  die();
+}
+
+/**
+ * Azione da effettuare quando è presente il cookie
+ * 
+ * Mostra correttamente la pagina richiesta
+ * 
+ */
+function bit_access_valid_cookie() {
+  echo "Eccoci qui! Ben fatto!";
+  die();
+}
+
+/**
+ * Azione da effettuare se non è presente il cookie
+ * Restituisce un 403 Forbidden
+ */
+function bit_access_invalid_cookie() {
+  header('HTTP/1.0 403 Forbidden');
+  echo 'Sorry... This page is forbidden (C)';
+  die();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////  REDIRECTING  ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Restituisce la url presso cui fare redirect in caso di passphrase corretta
+ */
+function bit_access_get_redirect() {
+  unset($_GET[PWD_ARGUMENT]);
+  $qstring = "?" . http_build_query($_GET); //ricostruiamo la qstring
+  $qstring = trim($qstring) === "?" ? "" : $qstring; //ricostruiamo la qstring
+  return "http://" . $_SERVER['SERVER_NAME'] . str_replace("?" . $_SERVER['QUERY_STRING'], "", $_SERVER['REQUEST_URI']) . $qstring;
+}
+
+/**
+ * Effettua un redirect tramite HEADER
+ * 
+ */
+function bit_access_redirect() {
+  header("Location: " . bit_access_get_redirect());
+  die();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////  GESTIONE DEI COOKIES  //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Restituisce TRUE se il cookie settato è corretto
+ * 
+ * @return type
+ */
+function bit_access_has_valid_cookie() {
+  return bit_access_get_cookie() ? bit_access_get_cookie() === VALID_COOKIE : FALSE;
+}
+
+/**
+ * Restituisce il valore del cookie di accesso
+ * 
+ * @return type
+ */
+function bit_access_get_cookie() {
+  return isset($_COOKIE[COOKIE_NAME]) ? $_COOKIE[COOKIE_NAME] : FALSE;
+}
+
+/**
+ * Setta il cookie corretto per visualizzare la pagina
+ * 
+ */
+function bit_access_set_cookie() {
+  setcookie(
+          COOKIE_NAME, VALID_COOKIE, time() + (10 * 365 * 24 * 60 * 60)
+  );
+}
+
